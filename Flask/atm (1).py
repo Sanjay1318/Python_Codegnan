864734cc5eb3d1d2b1cf226996cd2e0328e12f12
Sanjay1318 <sanjaychari999@gmail.com>
2026-05-01 12:15:51 +0530

    files.....

--- Flask/atm (1).py
+++ Flask/atm (1).py
@@ -117,6 +117,121 @@ def deposite():
 
     return render_template("deposite.html")
 
+@atm_o.route("/withdraw",methods=["GET","POST"])
+def withdraw():
+    if "ac_no" not in session:
+        return redirect(url_for("ac_no"))
+    
+    elif "username" not in session:
+        return redirect(url_for("pin"))
+    
+    ac_no=session.get("ac_no")
+    cursr.execute(""" select pin from user_data where ac_no=%s;""",(ac_no,))
+    data=cursr.fetchone()
+    
+    
+    if request.method=="POST":
+        w_pin=request.form["w_pin"]
+        
+
+        if int(w_pin)==int(data[0]):
+
+            return render_template("withdrawl_amount.html")
+        else:
+            return render_template("w_pin.html",info="wrong pin entered")
+
+    
+    return render_template("w_pin.html")
+
+@atm_o.route("/withdraw_amount",methods=["GET","POST"])
+def withdraw_amount():
+    if "ac_no" not in session:
+        return redirect(url_for("ac_no"))
+    
+    elif "username" not in session:
+        return redirect(url_for("pin"))
+    
+    ac_no=session.get("ac_no")
+    cursr.execute(""" select balance from user_data where ac_no=%s;""",(ac_no,))
+    data=cursr.fetchone()
+    if request.method=="POST" and request.form["w_form"] == "w_form" :
+        w_amount=request.form["w_amount"]
+
+        if float(w_amount) <= float(data[0]):
+
+            cursr.execute(""" update user_data set balance=balance-%s where ac_no=%s;""",(w_amount,ac_no))
+            db_connctr.commit()
+
+            cursr.execute(""" select balance from user_data where ac_no=%s;""",(ac_no,))
+            u_balance=cursr.fetchone()
+            session["w_amount"]=w_amount
+            session["balance"]=u_balance[0]
+
+            return redirect(url_for("withdraw_success"))
+        else:
+            return render_template("withdrawl_amount.html",info="* insufficient funds ")
+        
+@atm_o.route("/withdraw_success")
+def withdraw_success():
+    if "ac_no" not in session:
+        return redirect(url_for("ac_no"))
+    
+    elif "username" not in session:
+        return redirect(url_for("pin"))
+
+    return render_template(
+        "notification_w.html",username=session.get("username"),w_amount=session.get("w_amount"),balance=session.get("balance"))
+
+    
+@atm_o.route("/update_pin",methods=["get","post"])
+def change_pin():
+    if "ac_no" not in session:
+        return redirect(url_for("ac_no"))
+    
+    elif "username" not in session:
+        return redirect(url_for("pin"))
+    ac_no=session.get("ac_no")
+    cursr.execute(""" select pin from user_data where ac_no=%s;""",(ac_no,))
+    data=cursr.fetchone()
+
+    if request.method=="POST":
+         if request.form["c_pin"]=="c_pin":
+             ch_pin=request.form["ch_pin"]
+             if int(ch_pin)==int(data[0]):
+                 
+                 return render_template("n_pin.html")
+             else:
+                 return render_template("u_pin.html",info="*wrong pin entered")
+             
+
+
+    return render_template("u_pin.html")
+
+    
+@atm_o.route("/update_pin2",methods=["get","post"])
+def  change_pin2():
+    if "ac_no" not in session:
+        return redirect(url_for("ac_no"))
+    
+    elif "username" not in session:
+        return redirect(url_for("pin"))
+    ac_no=session.get("ac_no")
+
+    
+    if request.form["u_pin"]=="u_pin":
+             pin1=request.form["ch_pin1"]
+             pin2=request.form["ch_pin2"]
+             if pin1==pin2:
+                 cursr.execute("""update user_data set pin=%s  where ac_no=%s;""",(pin2,ac_no))
+                 db_connctr.commit()
+                 session.clear()
+                 return render_template("account.html",info="*password updated sucessfully plz login with new pin")
+             
+             else:
+                 return render_template("n_pin.html",info="* not same values in both ")
+                 
+             
+    
 
 
 @atm_o.route("/logout")
